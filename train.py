3a71f40f99e8724b5f3e5cb5d299de1e730a26d3
Anuradha Weeraman <anuradha@weeraman.com>
2019-01-21 17:10:08 -0500

    Added DDPG implementation

---
+++ train.py
@@ -0,0 +1,75 @@
+import numpy as np
+import random
+import torch
+import matplotlib.pyplot as plt
+from unityagents import UnityEnvironment
+from ddpg_agent import Agent
+from collections import deque
+
+env = UnityEnvironment(file_name="/Users/anuradha/ninsei/udacity/reinforcement-learning-continuous-control/Reacher.app")
+
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents
+num_agents = len(env_info.agents)
+print('Number of agents:', num_agents)
+
+# size of each action
+action_size = brain.vector_action_space_size
+print('Size of each action:', action_size)
+
+# examine the state space
+states = env_info.vector_observations
+state_size = states.shape[1]
+print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+print('The state for the first agent looks like:', states[0])
+
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=1)
+
+def ddpg(n_episodes=2000, max_t=700):
+    env_info = env.reset(train_mode=True)[brain_name]
+    scores_deque = deque(maxlen=100)
+    scores = []
+    max_score = -np.Inf
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name]
+        state = env_info.vector_observations[0]
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state)
+            env_info = env.step(action)[brain_name]
+            reward = env_info.rewards[0]
+            next_state = env_info.vector_observations[0]
+            done = env_info.local_done[0]
+            score += reward
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                break 
+        scores_deque.append(score)
+        scores.append(score)
+        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
+        if i_episode % 100 == 0:
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
+        if np.mean(scores_deque)>=30.0:  # consider done when the average score reaches 30 or more
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_deque)))
+            torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+            break
+    return scores
+
+scores = ddpg()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
