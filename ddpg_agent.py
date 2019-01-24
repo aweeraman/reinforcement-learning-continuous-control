0d217ee40b16b1d4ccca512f2c2e1d95b5075308
Anuradha Weeraman <anuradha@weeraman.com>
2019-01-23 22:51:27 -0500

    Updated model, refactored code

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -14,7 +14,7 @@ BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
 LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 3e-4        # learning rate of the critic
+LR_CRITIC = 1e-4        # learning rate of the critic
 WEIGHT_DECAY = 0.0001   # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
--- train.py
+++ train.py
@@ -1,12 +1,12 @@
+from unityagents import UnityEnvironment
 import numpy as np
 import random
 import torch
 import matplotlib.pyplot as plt
-from unityagents import UnityEnvironment
 from ddpg_agent import Agent
 from collections import deque
 
-env = UnityEnvironment(file_name="/Users/anuradha/ninsei/udacity/reinforcement-learning-continuous-control/Reacher.app")
+env = UnityEnvironment(file_name='Reacher.app')
 
 # get the default brain
 brain_name = env.brain_names[0]
@@ -31,45 +31,47 @@ print('The state for the first agent looks like:', states[0])
 
 agent = Agent(state_size=state_size, action_size=action_size, random_seed=1)
 
-def ddpg(n_episodes=2000, max_t=700):
-    env_info = env.reset(train_mode=True)[brain_name]
+def ddpg(n_episodes=2000, max_t=20000):
+
     scores_deque = deque(maxlen=100)
-    scores = []
-    max_score = -np.Inf
+    total_scores = []
+
     for i_episode in range(1, n_episodes+1):
-        env_info = env.reset(train_mode=True)[brain_name]
-        state = env_info.vector_observations[0]
-        score = 0
+        env_info = env.reset(train_mode=True)[brain_name]      # reset the environment
+        state = env_info.vector_observations[0]                  # get the current state (for each agent)
+        agent.reset()
+        score = 0                          # initialize the score (for each agent)
+
         for t in range(max_t):
             action = agent.act(state)
             env_info = env.step(action)[brain_name]
-            reward = env_info.rewards[0]
-            next_state = env_info.vector_observations[0]
-            done = env_info.local_done[0]
-            score += reward
+            next_state = env_info.vector_observations[0]         # get next state (for each agent)
+            reward = env_info.rewards[0]                         # get reward (for each agent)
+            done = env_info.local_done[0]                        # see if episode finished
             agent.step(state, action, reward, next_state, done)
-            state = next_state
-            if done:
-                break 
+            score += reward                         # update the score (for each agent)
+            state = next_state                               # roll over states to next time step
+            if done:                                  # exit loop if episode finished
+                break
+
         scores_deque.append(score)
-        scores.append(score)
-        print('\rEpisode {}\tAverage Score: {:.2f}\tScore: {:.2f}'.format(i_episode, np.mean(scores_deque), score), end="")
+        total_scores.append(score)
+
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))
+
         if i_episode % 100 == 0:
             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)))   
+
         if np.mean(scores_deque)>=30.0:  # consider done when the average score reaches 30 or more
             print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_deque)))
             torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
             torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
             break
-    return scores
 
-scores = ddpg()
+    plt.plot(np.arange(1, len(total_scores)+1), total_scores)
+    plt.ylabel('Score')
+    plt.xlabel('Episode #')
+    plt.show()
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
-plt.plot(np.arange(1, len(scores)+1), scores)
-plt.ylabel('Score')
-plt.xlabel('Episode #')
-plt.show()
+ddpg()
